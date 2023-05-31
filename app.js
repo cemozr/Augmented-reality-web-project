32ae850f58666def2fcb4c9291741bf6b83ede13
cem ozer <cemicozer@gmail.com>
2023-05-31 23:47:14 +0300

    rotations and hit test added

--- app.js
+++ app.js
@@ -20,8 +20,16 @@ const search = () => {
 };
 
 import { ARButton } from "https://unpkg.com/three@0.126.0/examples/jsm/webxr/ARButton.js";
+
 let camera, scene, renderer;
-let loader; // we need to create a variable for a gltf model loader
+let reticle;
+let controller;
+let model;
+
+var mouseDown = false;
+var lastMouseX = null;
+var lastMouseY = null;
+var rotationSpeed = 0.02;
 
 init();
 animate();
@@ -48,6 +56,12 @@ function init() {
   const directionalLight = new THREE.DirectionalLight(0xffffff, 4.5);
   scene.add(directionalLight);
 
+  addReticle();
+  addModel();
+  controller = renderer.xr.getController(0);
+  controller.addEventListener("select", onSelect);
+  scene.add(controller);
+
   // var light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 4);
   // light.position.set(0.5, 1, 0.25);
   // scene.add(light);
@@ -55,45 +69,93 @@ function init() {
   // const light = new THREE.AmbientLight(0x404040, 15); // soft white light
   // scene.add(light);
 
-  // specify a model URL
-  const modelUrl = "armchair/scene.gltf";
-
-  // create a GLTF loader object. GLTF is a 3D model format usually called the "JPEG of 3D" because it is
-  // fast and efficient to use, which is ideal for the web
-  loader = new THREE.GLTFLoader();
-
-  // learn about other types of 3D model formats you can use here:
-  // https://threejs.org/docs/#manual/en/introduction/Loading-3D-models
-
-  // load the model
-  // loader takes in a few arguments loader(model url, onLoad callback, onProgress callback, onError callback)
-  loader.load(
-    // model URL
-    modelUrl,
-    // onLoad callback: what get's called once the full model has loaded
-    function (gltf) {
-      // gltf.scene contains the Three.js object group that represents the 3d object of the model
-      scene.add(gltf.scene);
-      console.log("Model added to scene");
-
-      // you can optionally change the position of the model
-      gltf.scene.position.z = -2; // negative Z moves the model in the opposite direction the camera is facing
-      gltf.scene.position.y = -1; // positive Y moves the model up
-      gltf.scene.position.x = 0; // positive X moves hte model to the right
-    },
-    // onProgress callback: optional function for showing progress on model load
-    function (xhr) {
-      // console.log((xhr.loaded / xhr.total * 100) + '% loaded' );
-    },
-    // onError callback
-    function (error) {
-      console.error(error);
-    }
-  );
-
-  document.body.appendChild(ARButton.createButton(renderer));
+  const button = ARButton.createButton(renderer, {
+    requiredFeatures: ["hit-test"],
+  });
+  document.body.appendChild(button);
+  renderer.domElement.style.display = "none";
 
   window.addEventListener("resize", onWindowResize, false);
+
+  document.addEventListener("mousedown", onMouseDown, false);
+  document.addEventListener("mouseup", onMouseUp, false);
+  document.addEventListener("mousemove", onMouseMove, false);
+
+  document.addEventListener("touchstart", onTouchStart, false);
+  document.addEventListener("touchend", onTouchEnd, false);
+  document.addEventListener("touchmove", onTouchMove, false);
+}
+function onMouseDown(event) {
+  event.preventDefault();
+  mouseDown = true;
+  // lastMouseY = event.clientY;
+  lastMouseX = event.clientX;
+}
+
+function onMouseUp(event) {
+  event.preventDefault();
+  mouseDown = false;
+}
+
+function onMouseMove(event) {
+  event.preventDefault();
+  if (!mouseDown) return;
+  // var deltaY = event.clientY - lastMouseY;
+  let deltaX = event.clientX - lastMouseX;
+
+  // model.rotation.x += deltaY * rotationSpeed;
+  model.rotation.y += deltaX * rotationSpeed;
+
+  // lastMouseY = event.clientY;
+  lastMouseX = event.clientX;
+}
+function onTouchStart(event) {
+  event.preventDefault();
+  mouseDown = true;
+  lastMouseX = event.touches[0].clientX;
+}
+
+function onTouchEnd(event) {
+  event.preventDefault();
+  mouseDown = false;
+}
+
+function onTouchMove(event) {
+  event.preventDefault();
+  if (!mouseDown) return;
+
+  var deltaX = event.touches[0].clientX - lastMouseX;
+  model.rotation.y += deltaX * rotationSpeed;
+
+  lastMouseX = event.touches[0].clientX;
+}
+function addReticle() {
+  const geometry = new THREE.RingBufferGeometry(0.15, 0.2, 32).rotateX(
+    -Math.PI / 2
+  );
+  const material = new THREE.MeshBasicMaterial();
+  reticle = new THREE.Mesh(geometry, material);
+  reticle.matrixAutoUpdate = false;
+  reticle.visible = false;
+  scene.add(reticle);
+}
+async function addModel() {
+  const modelUrl = "armchair/scene.gltf";
+  const loader = new THREE.GLTFLoader();
+  const gltf = await loader.loadAsync(modelUrl);
+  model = gltf.scene;
+  model.scale.multiplyScalar(2);
+  model.visible = false;
+  scene.add(model);
+}
+
+function onSelect() {
+  console.log("clicked");
+  if (reticle.visible && model) {
+    model.visible = true;
+    model.position.setFromMatrixPosition(reticle.matrix);
+    model.quaternion.setFromRotationMatrix(reticle.matrix);
+  }
 }
 
 function onWindowResize() {
@@ -107,6 +169,46 @@ function animate() {
   renderer.setAnimationLoop(render);
 }
 
-function render() {
-  renderer.render(scene, camera);
+let hitTestSource = null;
+let localSpace = null;
+let hitTestSourceInitialized = false;
+
+async function initializeHitTestSource() {
+  const session = renderer.xr.getSession();
+  const viewerSpace = await session.requestReferenceSpace("viewer");
+  hitTestSource = await session.requestHitTestSource({ space: viewerSpace });
+  console.log(hitTestSource);
+
+  localSpace = await session.requestReferenceSpace("local");
+  console.log(localSpace);
+
+  hitTestSourceInitialized = true;
+
+  session.addEventListener("end", () => {
+    hitTestSourceInitialized = false;
+    hitTestSource = null;
+  });
+}
+
+function render(timestamp, frame) {
+  if (frame) {
+    if (!hitTestSourceInitialized) {
+      initializeHitTestSource();
+    }
+    if (hitTestSourceInitialized) {
+      const hitTestResults = frame.getHitTestResults(hitTestSource);
+      // console.log(hitTestResults);
+
+      if (hitTestResults.length > 0) {
+        const hit = hitTestResults[0];
+        const pose = hit.getPose(localSpace);
+        reticle.visible = true;
+        reticle.matrix.fromArray(pose.transform.matrix);
+      } else {
+        reticle.visible = false;
+      }
+    }
+
+    renderer.render(scene, camera);
+  }
 }
